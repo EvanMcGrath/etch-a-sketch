3e4ddb41965b22b7db8c196a6fd8d18a1b56443c
Evan McGrath <evan.grath@gmail.com>
2022-12-18 20:37:27 -0500

    Refactor gridSizerAdd range eventhandler

--- scripts.js
+++ scripts.js
@@ -1,10 +1,52 @@
 
-document.addEventListener("DOMContentLoaded", () => {
-    
-    let gridContent = document.getElementById("gridContent");
+let gridContent = document.getElementById("gridContent");
+let inputVal = document.getElementById("gridSizer").value;
 
-    for (i = 0; i < 16; i++) {
-        let gridDiv = document.createElement("div");
-        gridContent.appendChild(gridDiv);
-    }
+gridSizer(inputVal);
+
+function gridSizer(val) {
+        for (i=0; i < (val*val); i++) {
+                let gridDiv = document.createElement("div");
+                gridContent.appendChild(gridDiv);
+        }
+}
+
+
+
+let gridInput = document.getElementById("gridSizer");
+gridInput.addEventListener("input", () => {
+       let inputVal = document.getElementById('gridSizer').value;
+
+       gridSizer(inputVal);
+
 })
+
+
+
+
+let gridDivs = gridContent.querySelectorAll("div");
+gridDivs.forEach((item) => {item.classList.add("gridDivBorder")});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//for (i = 0; i < (16*16); i++) {
+//        let gridDiv = document.createElement("div");
+//        gridContent.appendChild(gridDiv);
+//}
+    
